bf9a8f794ebb329b22af4243ebc8cdff61d4c223
Anthony McCaigue <ant.mccaigue@gmail.com>
2025-01-06 14:50:11 +0000

    update styling

--- packages/docusaurus/src/components/Homepage.js
+++ packages/docusaurus/src/components/Homepage.js
@@ -34,8 +34,8 @@ function IconContainer({ png, title, description, docsLink }) {
           </div>
         )}
         <div className={styles.textContainer}>
-          <h3 className={styles.title}>{title}</h3>
-          <p className={styles.description}>{description}</p>
+          <h3 className={styles.title} style={{textAlign: 'center'}}>{title}</h3>
+          <p className={styles.description} style={{textAlign: 'center'}}>{description}</p>
           <Link
             className={`${styles.button} ${styles.glassButton}`}
             to={docsLink}
